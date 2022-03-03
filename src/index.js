99b3b8b207fef55b2fff05a94ea647eb387ea660
Aishwarya S R <Aishwarya_S_R@mckinsey.com>
2022-03-03 12:06:47 +0530

    Initial Commit: setup configuration files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const env = require('dotenv');
+const { router } = require('./routes/todo.route');
+
+env.config();
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(bodyParser.json());
+app.use('/notes', router);
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
